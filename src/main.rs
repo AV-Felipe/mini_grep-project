bbf2060104ccd89834104fdc21a912aac012d7cd
AV-Felipe <aerogrado@hotmail.com>
2022-06-18 10:54:53 -0300

    feat: implement read to string from fs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    
+    // store arguments from execution call into a vector of strings
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
@@ -9,4 +12,10 @@ fn main() {
 
     println!("searching for {}", query);
     println!("in file {}", filename);
+
+    // use the second argument as a reference for file read operation
+    let content = fs::read_to_string(filename)
+        .expect("something went wrong reading the file");
+
+    println!("text content:\n{}", content);
 }
