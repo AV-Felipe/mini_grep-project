3250e7ecbf5d5ffea0364562e989f29b544b973b
AV-Felipe <aerogrado@hotmail.com>
2022-06-21 15:04:56 -0300

    chore: custom panic on insuficient arguments

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,37 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("searching for {}", query);
-    println!("in file {}", filename);
+    let config = Config::new(&args);
 
     // use the second argument as a reference for file read operation
-    let content = fs::read_to_string(filename)
+    let content = fs::read_to_string(config.filename)
         .expect("something went wrong reading the file");
 
     println!("text content:\n{}", content);
+}
+
+// by using a struct to store this two values, we convey that
+//they are related values and simplify the process of finding them
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+    
+        // here we opt on cloning the values received by reference, to avoid having
+        //to manage references lifetime - its a trade-off of performance for simplicity
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        println!("searching for {}", query);
+        println!("in file {}", filename);
+    
+        Config {query, filename}
+    }
 }
