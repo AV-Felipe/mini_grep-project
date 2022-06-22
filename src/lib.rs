173b8124ce02c988b1cfa63312ec0c2ff7745eb8
AV-Felipe <aerogrado@hotmail.com>
2022-06-22 16:56:26 -0300

    tdd: 2-make the test pass

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,16 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>>{
 // lifetime here is telling that the string slice we are returning is referencing
 //the content parameter, and must exist for as long as that parameter data exists
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -44,9 +53,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query,contents));
     }
