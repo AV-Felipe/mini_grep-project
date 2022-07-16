9a666741eeb35dfc2d1d904a87c5e5b16129f70b
AV-Felipe <aerogrado@gmail.com>
2022-07-16 12:33:03 -0300

    feat: use CASE_INSENSITIVE environment variable to determine how to perform the search

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,49 @@
-use std::fs;
+use std::env;
 use std::error::Error;
+use std::fs;
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-    
+
         // here we opt on cloning the values received by reference, to avoid having
         //to manage references lifetime - its a trade-off of performance for simplicity
         let query = args[1].clone();
         let filename = args[2].clone();
-    
-        Ok(Config {query, filename})
+
+        // if the env variable CASE_INSENSITIVE does not exists will get a value of true
+        // else we will get false, independent of its value
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 // this function returns a result tha may be an empty tuple, on ok, or a struct that implements the Error trait
 // because we can have objects of different types that implement the same trait, we must pass it as dyn
 // also, because we cannot know its size on compile time, we must set it in a Box<T> pointer (it will be stored on the heap)
-pub fn run (config: Config) -> Result<(), Box<dyn Error>>{
-
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // here we use the ? for returning the error, else the code goes on
     let content = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &content) {
+    let result = if config.case_sensitive {
+        search(&config.query, &content)
+    } else {
+        search_case_insensitive(&config.query, &content)
+    };
+
+    for line in result {
         println!("{}", line);
     }
 
@@ -39,7 +53,6 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>>{
 // lifetime here is telling that the string slice we are returning is referencing
 //the content parameter, and must exist for as long as that parameter data exists
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-
     let mut results = Vec::new();
 
     for line in contents.lines() {
@@ -52,7 +65,6 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-
     let mut results = Vec::new();
 
     let query = query.to_lowercase();
@@ -75,7 +87,7 @@ mod tests {
         let query = "duct";
         let contents = "Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query,contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
@@ -83,6 +95,9 @@ mod tests {
         let query = "rUsT";
         let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query,contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
